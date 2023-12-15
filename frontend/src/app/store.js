55b2885178fbbc2c099443e2bc6a322b4bd13e00
Daljit Gill <s.gilldaljit@gmail.com>
2023-12-15 16:51:11 -0500

    Added goalForm -connected goal logic -refomatted code base

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authslice';
+import goalReducer from '../features/goals/goalSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    goals: goalReducer
   },
 });
